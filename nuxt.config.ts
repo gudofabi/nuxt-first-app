fae6d59f610e6d85c02517fb9d90067efd7b4b59
Gudo Fabi <godofredocpanel@gmail.com>
2024-03-21 16:29:40 +0800

    fix: fetching data

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 import {resolve} from 'path'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Frontend Developer | Gudo Fabi'
+    }
+  },
   devtools: { enabled: true },
   modules: [
     '@nuxt/content', 
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,30 @@
 <script setup lang="ts">
+// const { data: branch, pending } = await useFetch(
+//   "https://novadeci-be-ecs-dev.yondu.net/membership/api/v1/maintenance/branches/dropdown?filter=external",
+//   { transform: (_branch) => (_branch as any).result }
+// );
+
+// const { data: branch, pending } = await useLazyFetch(
+//   "https://novadeci-be-ecs-dev.yondu.net/membership/api/v1/maintenance/branches/dropdown?filter=external",
+//   { transform: (_branch) => (_branch as any).result }
+// );
+
+const { data: branch, pending } = await useAsyncData("branch", () =>
+  $fetch(
+    "https://novadeci-be-ecs-dev.yondu.net/membership/api/v1/maintenance/branches/dropdown?filter=external"
+  )
+);
+
+const func_refresh = () => refreshNuxtData("branch");
+// console.log((branch.value as any)?.result);
+console.log(branch.value);
+
 const { sayHello } = useUtils();
 const { $sayHello } = useNuxtApp();
+
 sayHello();
 </script>
+
 <template>
   <Alert />
   <Profile />
@@ -12,6 +34,14 @@ sayHello();
     Main Page
     {{ $sayHello("Fabi") }}
     <img src="@/assets/img/man.png" alt="man floating" />
-    <ContentDoc />
+  </div>
+  <div>
+    <div class="p-5 rounded border border-gray-100">
+      {{ pending ? "Loading" : branch }}
+    </div>
+    <br />
+    <button @click="func_refresh" class="px-3 py-2 bg-indigo-300 rounded">
+      Refresh
+    </button>
   </div>
 </template>
